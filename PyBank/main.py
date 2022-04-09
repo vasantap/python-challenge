7d48a02a92194ae58c56a10cde4bb2b2af51789e
Vasanta <vasanta75@gmail.com>
2022-04-09 08:27:31 -0700

    Code clean up and added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,14 @@
 import csv
 import os
 
+#import and open given csv file
 csvpath = os.path.join("Resources","budget_data.csv")
-
 with open(csvpath) as file:
+#create a csv reader object to read the data    
     reader = csv.reader(file)
     header = next(reader)
+
+#initialize variable and create lists to store data
     total_months = 0
     total_revenue = 0
     revenue_change = 0
@@ -16,9 +19,15 @@ with open(csvpath) as file:
     min_revenue_change = 0
     max_revenue_change_date = ""
     min_revenue_change_date = ""
+
+#loop through each row of data to calculate total months, total revenue, and revenue change
     for row in reader:
-        total_months += 1
-        total_revenue += int(row[1])
+        total_months += 1 #for total months
+        total_revenue += int(row[1]) #for total revenue
+
+#for revenue change and revenue change list if its not
+#the first row of data calculate change and append to list
+#else set previous revenue to the first row of data
         if prev_revenue != 0:
             revenue_change = int(row[1]) - prev_revenue
             prev_revenue = int(row[1])
@@ -26,14 +35,18 @@ with open(csvpath) as file:
         else:
             prev_revenue = int(row[1])
 
+#calculate greatest increase and decrease in profits
         if revenue_change > max_revenue_change:
             max_revenue_change = revenue_change
             max_revenue_change_date = row[0]
         if revenue_change < min_revenue_change:
             min_revenue_change = revenue_change
             min_revenue_change_date = row[0]
-   
+
+#calculate average revenue change  
     revenue_change_avg = round(sum(revenue_change_list) / len(revenue_change_list),2)
+ 
+#print results to terminal 
     print("Financial Analysis")
     print("----------------------------")
     print(f"Total Months: {total_months}")
@@ -42,6 +55,7 @@ with open(csvpath) as file:
     print(f"Greatest Increase in Profits: {max_revenue_change_date} (${max_revenue_change})")
     print(f"Greatest Decrease in Profits: {min_revenue_change_date} (${min_revenue_change})")
 
+#create text file to write results to
     pathoutput = os.path.join("analysis","budget_data_output.txt")  
     with open(pathoutput, 'w') as file:
         file.write("Financial Analysis\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,39 +1,47 @@
 import os
 import csv
 
+#import and open csv file
 csvpath = os.path.join("Resources","election_data.csv")
-
 with open(csvpath) as file:
+    
+#create csv reader object to read the data
     csvreader = csv.reader(file, delimiter=",")
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
+
+#initialize variable and create lists to store data
     total_votes = 0
     candidates = []
     candidate_votes = {}
+
+#loop through each row of data to calculate total votes and candidate votes   
     for row in csvreader:
-        total_votes += 1
+        total_votes += 1 #total votes
+
+#if candidate is not in list add candidate and set vote count to 1 else add 1 to vote count
         if row[2] not in candidates:
             candidates.append(row[2])
             candidate_votes[row[2]] = 1
         else:
             candidate_votes[row[2]] += 1
-    print(f"Total Votes: {total_votes}")
-    print(f"Candidates: {candidates}")
-    print(f"Candidate Votes: {candidate_votes}")
+
+#print results to terminal
     winner = max(candidate_votes, key=candidate_votes.get)
-    print(f"Winner: {winner}")
-    print("-------------------------")
-    print(f"Election Results")
+    print(f"\nElection Results")
     print(f"-------------------------")
     print(f"Total Votes: {total_votes}")
     print(f"-------------------------")
     for candidate in candidates:
         votes = candidate_votes[candidate]
+#calculate percentage of votes 
         vote_percentage = float(votes) / float(total_votes) * 100
         print(f"{candidate}: {vote_percentage:.3f}% ({votes})")
     print(f"-------------------------")
     print(f"Winner: {winner}")
     print(f"-------------------------")
+
+#create text file to write results to
     output_file = os.path.join("analysis", "election_data.txt")
     with open(output_file, "w") as file:
         file.write(f"Election Results\n")
